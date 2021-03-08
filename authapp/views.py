bd6d34a949cc2095e94cc17de75cfe672ef47cce
Sergey <serega_17_94@mail.ru>
2021-03-09 02:23:16 +0300

    create base; edited forms, login, urls and view

--- authapp/views.py
+++ authapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
-from django.contrib import  messages
+from django.contrib import messages
 
-from authapp.forms import UserLoginFrom, UserCreationForm
+from authapp.forms import UserLoginForm, UserRegisterForm
+
 
 def login(request):
     if request.method == 'POST':
-        form = UserLoginFrom(data=request.POST)
+        form = UserLoginForm(data=request.POST)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
@@ -15,10 +16,28 @@ def login(request):
             if user and user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
     else:
-        form = UserLoginFrom()
+        form = UserLoginForm()
     context = {'form': form}
     return render(request, 'authapp/login.html', context)
 
+
 def register(request):
-    return render(request, 'authapp/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'authapp/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
