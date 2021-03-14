ffcde77feea057f620e1938da3b99123f1549470
Sergey <serega_17_94@mail.ru>
2021-03-14 21:50:53 +0300

    created controller for update users

--- admiapp/views.py
+++ admiapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from  authapp.models import User
-from admiapp.forms import UserAdminRegistrationForm
+from admiapp.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
@@ -26,9 +26,18 @@ def admin_users_create(request):
     context = {'form': form}
     return render(request, 'adminapp/admin-users-create.html', context)
 
-# UPDATE and DELETE
-def admin_users_update(request):
-    return render(request, 'adminapp/admin-users-update-delete.html')
+# UPDATE
+def admin_users_update(request, user_id):
+    user = User.objects.get(id=user_id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=user)
+    context = {'form': form, 'user': user}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 # DELETE
 def admin_users_delete(request):
--- authapp/views.py
+++ authapp/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from authapp.forms import UserLoginForm, UserRegisterForm,UserProfileForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from basketapp.models import Basket
 
 
