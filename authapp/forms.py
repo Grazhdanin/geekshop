ff20a12f96ee965182d207839b7c8ee65686b266
Sergey <serega_17_94@mail.ru>
2021-03-30 02:04:16 +0300

    Added account activation by e-mail.

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
 from authapp.models import User
+import hashlib
+import random
 
 
 class UserLoginForm(AuthenticationForm):
@@ -32,11 +34,18 @@ class UserRegisterForm(UserCreationForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
 
-
+    def save(self):
+        user = super(UserRegisterForm, self).save()
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
+        user.save()
+        return user
 
 
 class UserProfileForm(UserChangeForm):
     avatar = forms.ImageField(widget=forms.FileInput(), required=False)
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'avatar', 'username', 'email')
@@ -48,5 +57,3 @@ class UserProfileForm(UserChangeForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['avatar'].widget.attrs['class'] = 'custom-file-input'
-
-
--- authapp/urls.py
+++ authapp/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from authapp.views import login, register, logout, profile
-
+from authapp.views import logout, GeekLoginView, RegisterView, ProfileView
 
 app_name = 'authapp'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
+    path('login/', GeekLoginView, name='login'),
+    path('register/', RegisterView, name='register'),
     path('logout', logout, name='logout'),
-    path('profile', profile, name='profile')
-
+    path('profile', ProfileView, name='profile'),
+    path('verify/<str:email>/<str:activation_key>/', RegisterView.verify, name='verify'),
 ]
